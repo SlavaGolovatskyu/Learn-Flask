11a6d184f76454f1f04aeaceb40b26306bad4307
Slava <61500332+SlavaGolovatskyu@users.noreply.github.com>
2021-02-28 20:37:11 +0200

    first commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
