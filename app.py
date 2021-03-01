c462d5db0268bd5396d404c0973d3f86caa48d09
Slava <61500332+SlavaGolovatskyu@users.noreply.github.com>
2021-03-01 20:04:09 +0200

    update commit

--- app.py
+++ app.py
@@ -36,9 +36,14 @@ def about_page():
 
 @app.route('/posts')
 def posts():
-	articles = Article.query.order_by(Article.date).all()
-	articles.reverse()
+	articles = Article.query.order_by(Article.date.desc()).all()
 	return render_template('posts.html', articles=articles)
+
+
+@app.route('/posts/<int:id>')
+def post_detail(id):
+	article = Article.query.get(id)
+	return render_template('post_detail.html', article=article)
 
 
 @app.route('/create-article', methods=['POST', 'GET'])
@@ -53,7 +58,7 @@ def create_article():
 		try:
 			db.session.add(article)
 			db.session.commit()
-			return redirect('/')
+			return redirect('/posts')
 		except:
 			return 'При добавлении статьи произошла ошибка'
 	else:
