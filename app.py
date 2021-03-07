ceebc3a1c78503afe43e77b6f290eaf8b940e0eb
Slava <61500332+SlavaGolovatskyu@users.noreply.github.com>
2021-03-07 21:18:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from validation.check_len import *
 from datetime import datetime
 
 from flask import (
-	Flask, 
-	render_template, 
-	url_for, 
-	request, 
-	redirect
+    Flask, 
+    render_template, 
+    url_for, 
+    request, 
+    redirect
 )
 
 from flask_sqlalchemy import SQLAlchemy
